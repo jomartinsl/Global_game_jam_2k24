d5e5adff233c4e2bfd6d73d79dc12f0bb339750b
Martin <martinolsbo1@gmail.com>
2024-01-28 11:47:35 +0100

    Fjervåpen endring
    Lagt til lyd effekt, synkronisert lengdene på animasjon og lyd, begrensa angrepsmulighet til hvert 0.45 sek og lagt til at du må sleppe muse knappen for å kunne bruke han igjenn.

--- Scripts/Feather.gd
+++ Scripts/Feather.gd
@@ -4,24 +4,30 @@ const SPEED = 300
 var velocity_percent = Vector2.ZERO
 var direction = Vector2(0, 0)
 
+#States
+var attacking = false
+var time = 0
 
 func _ready():
 	get_node("Hitbox").disabled = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	look_at(get_global_mouse_position())
-	_animate()
-	#get_node("Hitbox").disabled = false
+	_animate(delta)
 
-	
-func _animate():
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+func _animate(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and !attacking:
 		get_node("Animation").play("Feather")
 		get_node("Hitbox").disabled = false
-	else:
-		get_node("Animation").stop()
-		get_node("Hitbox").disabled = true
-
-	# Look at the mouse position regardless of button press
+		$TickleSound.play()
+		attacking = true
+		time = 0
+	elif time >= .45:
+		if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			get_node("Animation").stop()
+			get_node("Hitbox").disabled = true
+			attacking = false
+	elif attacking:
+		time += delta
+		print(time)
 	look_at(get_global_mouse_position())
